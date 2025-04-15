34175bbba8081ffdf4cbc0e8b3281805a254165e
mustafa-tola <mustafatola02@gmail.com>
2025-04-15 20:49:50 +0500

    Updated bot

--- c.js
+++ c.js
@@ -9,7 +9,7 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.route("/", (req,res,next) => {
+app.get("/", (req,res,next) => {
   return res.status(200).json({status: "success"})
 })
 
